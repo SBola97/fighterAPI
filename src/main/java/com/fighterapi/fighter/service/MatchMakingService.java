879844160f7f15c47a15153820eb1b3432a0a6d3
SBola97 <sebassenk1997@gmail.com>
2025-04-18 04:43:39 +0200

    Code cleanup

--- src/main/java/com/fighterapi/fighter/service/MatchMakingService.java
+++ src/main/java/com/fighterapi/fighter/service/MatchMakingService.java
@@ -35,29 +35,12 @@ public class MatchMakingService {
 
         List<Fighter> candidates = fighterRepository.findAll();
 
-        var fighterStrategy = strategies.stream()
-                .filter(strategy -> strategy.supports(sourceFighter))
-                .findFirst()
-                .orElseThrow(() -> new StrategyNotFoundException("Matchmaking not found for this fighter"));
+        var fighterStrategy = validateFighterStrategy(sourceFighter);
 
         return candidates.stream()
                 .filter(candidate -> candidate.getId() != sourceFighter.getId())
                 .filter(candidate -> fighterStrategy.isMatch(sourceFighter, candidate))
                 .map(candidate -> mapper.fighterToFighterDTO(candidate))
-                .toList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<FighterDTO> findMatch(int fighterId) {
-        var sourceFighter = fighterRepository.findById(fighterId)
-                .orElseThrow(() -> new FighterNotFoundException("Fighter not found to matchmaking"));
-        List<Fighter> candidates = fighterRepository.findAll();
-
-        return candidates
-                .stream()
-                .filter(fighter -> fighter.getId() != fighterId)
-                .filter(fighter -> fighter.getType().equals(sourceFighter.getType()))
-                .map(fighter -> mapper.fighterToFighterDTO(fighter))
                 .toList();
     }
 
@@ -72,5 +55,13 @@ public class MatchMakingService {
                 .map(fighter -> mapper.fighterToFighterDTO(fighter))
                 .toList();
     }
+
+    private MatchMakingStrategy validateFighterStrategy(Fighter sourceFighter) {
+        return strategies.stream()
+                .filter(strategy -> strategy.supports(sourceFighter))
+                .findFirst()
+                .orElseThrow(() -> new StrategyNotFoundException("Matchmaking not found for this fighter"));
+    }
+
 }
 
